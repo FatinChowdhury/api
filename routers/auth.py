cfe302e8af2c3d62ea5d0191e9e354c752522a58
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-05-31 23:49:50 -0400

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,116 @@
-from fastAPI import APIRouter
+from typing import Annotated
+from datetime import timedelta, datetime
+from fastAPI import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
 
-router = APIRouter()
+router = APIRouter(
+    prefix = '/auth',
+    tags = ['auth']
+)
 
-@router.get("/auth/")
+SECRET_KEY = '2834y3b45uy45b4b534ihuifg893454hbb'
+ALGORITHM = 'HS256'
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl = 'auth/token')
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db        # from def get_db() till this line here, code executed before sending response
+    finally:
+        db.close()      # executed after response delivered
+
+        # makes fastapi faster (fetch info from db, return it to client, close the connection to db after)
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user     # went from True to user or else there will be internal server error
+
+
+def create_access_token(username: str, user_id: int, role: str, expires_delta: timedelta):
+    encode = {'sub': username, 'id': user_id, 'role': role}
+    expires = datetime.utcnow() + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm = ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithm=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        user_role: str = payload.get('role')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid Token')
+        return {'username': username, 'id': user_id, 'user_role': user_role}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid Token')
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_users_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active = True
+    )
+
+    # run main:app
+    # if u put a password in "create user", u will see in the response body
+    # that the password is hashed
+
+    db.add(create_user_model)
+    db.commit() # call transaction to db to add create user model
+
+    # run using uvicorn, create user with random names, password, etc
+    # will get 201 response (null), so run sqlite3 todoapp.db
+    # then type select * from users;
+    # should get ur info
+
+@router.post("/token", response_model = Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depemds()],
+db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid Token')
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
+
+    # now, run using uvicorn
+    # we see that we will get our token back
+    # now, time to make class Token and modifying this router.post thing (Adding response)
+    # and modify return statement
+
+    # return token
+    return {'access_token':token, 'token_type':'bearer'}
+
+
